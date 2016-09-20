46973ac52b07e35a1ed87259503970e5ae3e0529
Eric Kinzie <eric@qosient.com>
2016-09-20 15:10:32 -0400

    release 5.0.3 update Tue Sep 20 15:10:37 EDT 2016

--- include/argus_out.h
+++ include/argus_out.h
@@ -22,9 +22,9 @@
  */
 
 /* 
- * $Id: //depot/gargoyle/argus/include/argus_out.h#12 $
- * $DateTime: 2016/09/13 16:02:14 $
- * $Change: 3181 $
+ * $Id: //depot/gargoyle/argus/include/argus_out.h#13 $
+ * $DateTime: 2016/09/18 16:32:53 $
+ * $Change: 3186 $
  */
 
 
