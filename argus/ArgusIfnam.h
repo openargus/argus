68cd57a941d2c89112b3b58fc00d3d72af3c7dca
Eric Kinzie <eric@qosient.com>
2017-02-28 15:15:59 -0500

    choose abbreviated interface name for srcid
    Add file ArgusIfnam.c with logic to choose a unique four-character
    interface name abbreviation to use in the source ID.  If an
    abbreviation has already been used, advance the first letter to the
    next in the alphabet.  For example, if "e123" has been used, the
    next interface name that is abbreviated to the same string will be
    replaced with "f123".  Windows "transport names" are handled by
    skipping over the leading backslash and truncating the name to
    remove the trailing brace.
    
    Trac ticket #33

---
+++ argus/ArgusIfnam.h
@@ -0,0 +1,16 @@
+#ifndef __ARGUS_IFNAM_H
+# define __ARGUS_IFNAM_H
+
+# ifdef HAVE_CONFIG_H
+#  include "argus_config.h"
+# endif
+
+# include "argus_compat.h"
+# include "ArgusModeler.h"
+# include "ArgusSource.h"
+
+int
+shortname_ethdev_unique(char *in, char *out, size_t outlen,
+                        struct ArgusListStruct *ArgusDeviceList);
+
+#endif
