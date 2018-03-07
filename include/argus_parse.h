946b8e9c8b67296905aa42510513738cf6dfc092
Eric Kinzie <eric@qosient.com>
2018-03-07 15:28:00 -0500

    remove unused function ArgusNtoH
    Argus generates flow records but it does not consume them.

--- include/argus_parse.h
+++ include/argus_parse.h
@@ -295,7 +295,6 @@ int check_time_format (struct tm *tm, char *str);
 int parseTime (struct tm *, struct tm *, char *);
 
 #if defined(_LITTLE_ENDIAN)
-void ArgusNtoH (struct ArgusRecord *argus);
 void ArgusHtoN (struct ArgusRecord *argus);
 #endif
 
@@ -498,7 +497,6 @@ extern int check_time_format (struct tm *tm, char *str);
 extern int parseTime (struct tm *, struct tm *, char *);
 
 #if defined(_LITTLE_ENDIAN)
-extern void ArgusNtoH (struct ArgusRecord *argus);
 extern void ArgusHtoN (struct ArgusRecord *argus);
 #endif
 
