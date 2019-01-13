dcba5c9ba3f6bd4a12609af8d62a7acd2f09149b
Carter Bullard <carter@qosient.com>
2019-01-13 15:57:34 -0500

    Check that sources that have no packet sources, don't init again.

--- argus/ArgusSource.c
+++ argus/ArgusSource.c
@@ -855,6 +855,7 @@ ArgusInitSource (struct ArgusSourceStruct *src)
       retn = 1;
 
    } else {
+      src->status |= ARGUS_NOSOURCES;
 #ifdef ARGUSDEBUG
       ArgusDebug (1, "ArgusInitSource: no packet sources for device %s.",
                   src->ArgusDeviceStr ? src->ArgusDeviceStr : "(unknown)");
@@ -4990,7 +4991,7 @@ ArgusSourceProcess (struct ArgusSourceStruct *stask)
                      ArgusFree(src);
 
                } else {
-                  if (!(src->status & ARGUS_LAUNCHED)) {
+                  if (!(src->status & (ARGUS_LAUNCHED | ARGUS_NOSOURCES))) {
                      if (ArgusInitSource (src) > 0) {
                         if (new_gid > 0) {
                            if (setgid(new_gid) < 0)
