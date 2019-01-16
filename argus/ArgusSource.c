b3ae7701a1b599c9247d229519c0c99a32083103
Carter Bullard <carter@qosient.com>
2019-01-15 19:28:47 -0500

    replace obsoleted pcap_lookupdev() with name from first device in pcap_findalldevs().

--- argus/ArgusSource.c
+++ argus/ArgusSource.c
@@ -1271,6 +1271,7 @@ setArgusDevice (struct ArgusSourceStruct *src, char *cmd, int type, int mode)
                                              dev->name, inf);
                                  }
                               }
+                              dev->inf = ArgusGenerateMarInfStruct(dev, d);
                            }
 
                            switch (status) {
