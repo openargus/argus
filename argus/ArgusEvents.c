84c71bc3385c22c9abd3d92d955e4f84f4522917
Carter Bullard <carter@qosient.com>
2019-11-06 13:15:11 -0500

    Mods to fix IPv6 fragment tracking and to deal with segfault on event queue on close.

--- argus/ArgusEvents.c
+++ argus/ArgusEvents.c
@@ -89,12 +89,14 @@ ArgusDeleteEvents (struct ArgusEventsStruct *events)
 {
    struct ArgusEventsStruct *retn = NULL;
 
-   pthread_mutex_destroy(&events->lock);
-   if (events->ArgusEventsList != NULL) {
-      ArgusDeleteList (events->ArgusEventsList, ARGUS_EVENT_LIST);
-      events->ArgusEventsList =  NULL;
+   if (events != NULL) {
+      pthread_mutex_destroy(&events->lock);
+      if (events->ArgusEventsList != NULL) {
+         ArgusDeleteList (events->ArgusEventsList, ARGUS_EVENT_LIST);
+         events->ArgusEventsList =  NULL;
+      }
+      ArgusFree (events);
    }
-   ArgusFree (events);
 
 #ifdef ARGUSDEBUG
    ArgusDebug (1, "ArgusDeleteEvents() returning retn 0x%x\n", retn);
