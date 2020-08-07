0c8d4aaf64627b9448bc2b113adf639163d95483
Carter Bullard <carter@qosient.com>
2020-08-07 12:42:01 -0400

    Mods to add VxLanStruct support for output .

--- include/argus_out.h
+++ include/argus_out.h
@@ -666,9 +666,7 @@ struct ArgusMacStruct {
 
 struct ArgusVxLanStruct {
    struct ArgusDSRHeader hdr;
-   unsigned char flgs;
-   unsigned char res[3];
-   unsigned int vni;
+   unsigned int svnid, dvnid;
 };
    
 struct ArgusVlanStruct {
