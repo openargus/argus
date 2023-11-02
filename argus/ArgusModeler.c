5b8ef1df7dbf2f83d2928a2c859f5023bf90d408
Carter Bullard <carter@qosient.com>
2023-11-02 12:56:33 -0400

    Fix issue with comments

--- argus/ArgusModeler.c
+++ argus/ArgusModeler.c
@@ -953,7 +953,6 @@ ArgusProcessUdpHdr (struct ArgusModelerStruct *model, struct ip *ip, int length)
             len += sizeof (*up);
 
             if (STRUCTCAPTURED(model, *ipv6)) {
-/*
                int isipv6 = 0;
                if ((isipv6 = (ipv6->ip6_vfc & IPV6_VERSION_MASK)) == IPV6_VERSION) {
                   retn = ETHERTYPE_IPV6;
@@ -1015,7 +1014,6 @@ ArgusProcessUdpHdr (struct ArgusModelerStruct *model, struct ip *ip, int length)
                      }
                   }
                }
-*/
             }
          }
 */
