f7c70827c88562bfdf023164cef6a893ec66da32
Carter Bullard <carter@qosient.com>
2024-01-29 12:37:01 -0500

    Update for argus-5.0 copyright and disclaimer ...

--- events/argus-ipaddr.pl
+++ events/argus-ipaddr.pl
@@ -1,6 +1,6 @@
 #!@PERLBIN@
 #
-#  Gargoyle Software.  Argus Event scripts - ip addr
+#  Argus-5.0 Software.  Argus Event scripts - ip addr
 #  Copyright (c) 2000-2024 QoSient, LLC
 #  All rights reserved.
 #
